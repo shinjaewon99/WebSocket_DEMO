0a9d115a87560a58bdf082ef4ce108e291d3057d
shinjaewon99 <tlswodnjs99@naver.com>
2025-01-27 17:50:54 +0900

    feat : 웹소켓과 stomp 프로토콜 사용

--- web-socket/src/main/java/com/websocket/web_socket/config/WebSocketConfig.java
+++ web-socket/src/main/java/com/websocket/web_socket/config/WebSocketConfig.java
@@ -2,19 +2,31 @@ package com.websocket.web_socket.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocket
 @RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketConfigurer {
-    private final WebSocketHandler webSocketHandler;
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "/ws-endpoint")
-                .setAllowedOrigins("*");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub"); // 특정 토픽을 구독
+        registry.setApplicationDestinationPrefixes("/pub");    // 메시지를 발행
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/stomp")
+//                .setAllowedOriginPatterns("*")
+//                .withSockJS();
+
+        // 테스트 시 에러로 인하여 .withSockJs() 제외 엔드포인트 추가
+        // 실제 환경에선 불필요
+        registry.addEndpoint("/stomp")
+                .setAllowedOriginPatterns("*");
     }
 }
