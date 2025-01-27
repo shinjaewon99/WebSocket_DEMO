6a0c064218943090b756b16bc4e3257c27425a9a
shinjaewon99 <tlswodnjs99@naver.com>
2025-01-27 18:01:56 +0900

    feat : 스케줄링 사용 추가

--- web-socket/src/main/java/com/websocket/web_socket/WebSocketApplication.java
+++ web-socket/src/main/java/com/websocket/web_socket/WebSocketApplication.java
@@ -2,8 +2,10 @@ package com.websocket.web_socket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling        // 스케줄링 사용
 public class WebSocketApplication {
 
 	public static void main(String[] args) {
--- web-socket/src/main/java/com/websocket/web_socket/config/WebSocketConfig.java
+++ web-socket/src/main/java/com/websocket/web_socket/config/WebSocketConfig.java
@@ -3,29 +3,27 @@ package com.websocket.web_socket.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
+@EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+    // TODO : Client는 메시지를 수신만 한다.
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/sub"); // 특정 토픽을 구독
-        registry.setApplicationDestinationPrefixes("/pub");    // 메시지를 발행
+
+        /* 클라이언트가 메시지를 발행(pub)하지 않으니 발행 경로 추가 X */
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/stomp")
-//                .setAllowedOriginPatterns("*")
-//                .withSockJS();
-
-        // 테스트 시 에러로 인하여 .withSockJs() 제외 엔드포인트 추가
-        // 실제 환경에선 불필요
+        registry.addEndpoint("/stomp")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
         registry.addEndpoint("/stomp")
                 .setAllowedOriginPatterns("*");
     }
